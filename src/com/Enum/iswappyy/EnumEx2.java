a081b1ab4606fb6585285e0a0a0b91bed93fd9ec
Iswappyy <swadipthawkar0902@gmail.com>
2022-03-14 11:59:14 +0530

    Enum changes

---
+++ src/com/Enum/iswappyy/EnumEx2.java
@@ -0,0 +1,30 @@
+package com.Enum.iswappyy;
+
+enum Days{MONDAY,TUESDAY,WEDNESDAY,THIRSDAY,FRIDAY,SATURDAY,SUNDAY}
+
+public class EnumEx2 {
+	
+	public static void main(String[] args) {
+		Days days = Days.MONDAY;
+		
+	switch(days) {
+	case MONDAY:
+		System.out.println("Today is monday");
+	break;
+	
+	case FRIDAY :
+		System.out.println("Today is Friday");
+		break;
+		
+	default:
+		System.out.println("Invalid entry..!");
+	
+	}
+	System.out.println("##########################################################");
+	 
+	for( Days day : Days.values()) {
+		System.out.println(day);
+	}
+
+}
+}
